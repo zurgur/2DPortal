ebec2cffa318951b2484adef23b834016de3a5c6
meatyminx <flexayoung@gmail.com>
2018-06-10 20:00:13 +0200

    mend

---
+++ Assets/Scripts/Engine/PlatformDestroyer.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+	public GameObject destructionPoint;
+	private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+		destructionPoint = GameObject.Find ("PlatformDestructionPoint");
+		platformWidth = gameObject.GetComponent<BoxCollider2D>().size.x;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x + platformWidth / 2 < destructionPoint.transform.position.x) {
+			gameObject.SetActive(false);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/Engine/PlayerController.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	private float moveSpeed = 10;
+	private float jumpForce = 7;
+	public bool isMirrored;
+
+	private Rigidbody2D myRigidBody;
+
+	public bool grounded;
+	public LayerMask whatIsGround; 
+
+	private Collider2D myCollider;
+
+	private Animator myAnimator;
+
+	// Use this for initialization
+	void Start () {
+		myRigidBody = GetComponent<Rigidbody2D>();
+
+		myCollider = GetComponent<Collider2D>();
+
+		myAnimator = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		grounded = Physics2D.IsTouchingLayers(myCollider, whatIsGround);
+
+		myRigidBody.velocity = new Vector2(moveSpeed, myRigidBody.velocity.y);
+ 
+		if(grounded && Input.GetKeyDown(KeyCode.Space)) {
+			if(!isMirrored) {
+				myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce);
+			} else {
+				myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, -jumpForce);
+			}
+		}
+
+		myAnimator.SetFloat ( "Speed", myRigidBody.velocity.x );
+		myAnimator.SetBool ( "Grounded", grounded); 
+		
+	}
+}
