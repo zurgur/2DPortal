17cc8736cdbcef4112f716b87d634157dbe5b891
meatyminx <flexayoung@gmail.com>
2018-06-10 16:20:06 +0200

    Basic engine setup, regenarative ground

---
+++ Assets/Scripts/PlatformDestroyer.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformDestroyer : MonoBehaviour {
+	public GameObject destructionPoint;
+	private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+		destructionPoint = GameObject.Find ("PlatformDestructionPoint");
+		platformWidth = gameObject.GetComponent<BoxCollider2D>().size.x;
+		Debug.Log(destructionPoint);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x + platformWidth / 2 < destructionPoint.transform.position.x) {
+			Destroy(gameObject);
+		}
+		
+	}
+}
---
+++ Assets/Scripts/PlatformGenerator.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlatformGenerator : MonoBehaviour {
+
+	public GameObject thePlatform;
+	public  Transform generationPoint;
+
+	private float platformWidth;
+
+	// Use this for initialization
+	void Start () {
+		platformWidth = thePlatform.GetComponent<BoxCollider2D>().size.x;
+		transform.position = new Vector3(platformWidth/2, 0, transform.position.z);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(transform.position.x < generationPoint.position.x) {
+			transform.position = new Vector3(transform.position.x + platformWidth / 2, transform.position.y, transform.position.z);
+			Instantiate (thePlatform, transform.position, transform.rotation );
+			transform.position = new Vector3(transform.position.x + platformWidth / 2, transform.position.y, transform.position.z);
+
+		}
+	}
+}
