de7abbd69640f0a95d3fcbbb59514534b02a59ce
zurgur <harryfreyr@gmail.com>
2018-06-11 13:00:25 +0000

    points text conected

---
+++ Assets/Scripts/Menus/ShowPoints.cs
@@ -0,0 +1,18 @@
+﻿using TMPro;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowPoints : MonoBehaviour {
+
+    [SerializeField]
+    private string player;
+
+    [SerializeField]
+    private TextMeshProUGUI points;
+
+	void Start () {
+        int currentPoints = PlayerPrefs.GetInt(player);
+        points.text = currentPoints.ToString();
+	}
+
+}
