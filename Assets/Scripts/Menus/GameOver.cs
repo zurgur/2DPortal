c43f7df67e71228cf558c0b836b9195492cad084
zurgur <harryfreyr@gmail.com>
2018-06-11 13:27:38 +0000

    game over screen

---
+++ Assets/Scripts/Menus/GameOver.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOver : MonoBehaviour
+{
+
+    public static bool GameIsOver = false;
+
+    public GameObject gameOverMenuUI;
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (GameIsOver)
+        {
+            gameOverMenuUI.SetActive(true);
+            Time.timeScale = 0f;
+            GameIsOver = true;
+        }
+    }
+    public void Resume()
+    {
+        gameOverMenuUI.SetActive(false);
+        Time.timeScale = 1f;
+        GameIsOver = false;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
+}
