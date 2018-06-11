a4e9c2d6128bfd8c4aafa15b403398fc609f3255
zurgur <harryfreyr@gmail.com>
2018-06-11 09:51:57 +0000

    health bar

---
+++ Assets/Scripts/Engine/PlayerHealth.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerHealth : MonoBehaviour {
+
+    [SerializeField]
+    private int health;
+
+    [SerializeField]
+    private Slider healthBar;
+
+    void Start()
+    {
+        HurtPlayer(50);
+    }
+
+    public void HurtPlayer(int damage)
+    {
+        health -= damage;
+        if (health<=0)
+        {
+            Debug.Log("dead need to implament deth screen");
+        }
+        healthBar.value = health;
+    }
+	
+}
