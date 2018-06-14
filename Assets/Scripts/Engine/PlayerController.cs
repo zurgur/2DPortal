b278f016bdb9a0017c88ca32d00ba4999de56771
meatyminx <flexayoung@gmail.com>
2018-06-14 12:14:21 +0200

    Improved platforms

--- Assets/Scripts/Engine/PlayerController.cs
+++ Assets/Scripts/Engine/PlayerController.cs
@@ -4,11 +4,11 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour {
 
-	private float moveSpeed = 10;
-	private float jumpForce = 7;
+	private float moveSpeed = 12;
+	private float jumpForce = 12;
 	public bool isMirrored;
 
-	private float jumpTime = 0.5f;
+	private float jumpTime = 0.25f;
 	private float jumpTimeCounter;
 
 	private Rigidbody2D myRigidBody;
@@ -44,11 +44,19 @@ public class PlayerController : MonoBehaviour {
 			}
 		}
 
-		if(Input.GetKeyDown(KeyCode.Space)) {
+		if(Input.GetKey(KeyCode.Space)) {
 			if(jumpTimeCounter > 0){
 				myRigidBody.velocity = new Vector2(myRigidBody.velocity.x, jumpForce); 
 				jumpTimeCounter -= Time.deltaTime;
 			}
+		}
+
+		if(Input.GetKeyUp(KeyCode.Space)) {
+			jumpTimeCounter = 0;
+		}
+
+		if(grounded) {
+			jumpTimeCounter = jumpTime;
 		}
  
 		myAnimator.SetFloat ( "Speed", myRigidBody.velocity.x );
