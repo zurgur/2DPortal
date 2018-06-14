a19874b8ff3984e92ba7be3bef2f19b07e14da06
zurgur <harryfreyr@gmail.com>
2018-06-14 08:25:20 +0000

    śtore ui

---
+++ Assets/Scripts/Menus/LevelSelect.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class LevelSelect : MonoBehaviour {
+
+    public Button[] itemButtons;
+
+	// Use this for initialization
+	void Start () {
+        for (int i = 1; i < itemButtons.Length; i++)
+        {
+           //  itemButtons[i].interactable = PlayerPrefsManager.IsLevelUnlocked(i);
+            
+        }
+	}
+	
+}
