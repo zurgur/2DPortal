5ca7f34480835c3281357aaf878599c8277af39f
zurgur <harryfreyr@gmail.com>
2018-06-17 16:39:38 +0000

    show new max distance

--- Assets/Scripts/Engine/DistanceManager.cs
+++ Assets/Scripts/Engine/DistanceManager.cs
@@ -1,23 +1,46 @@
 ﻿
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
+
 
 public class DistanceManager : MonoBehaviour {
 
     private float maxDistance;
 
-    
-	void Start () {
+    [SerializeField]
+    private GameObject panel;
+
+    [SerializeField]
+    private TextMeshProUGUI text;
+
+    private float timeToShow = 10;
+
+    void Start () {
         maxDistance = PlayerPrefsManager.getDistance();
 	}
 	
 	// Update is called once per frame
 	void Update () {
         float distance = Vector3.Distance(this.transform.position, new Vector3(0, 0, 0));
+
+        if(distance > maxDistance && timeToShow > 0)
+        {
+            text.text = distance.ToString();
+            panel.SetActive(true);
+            timeToShow -= Time.deltaTime;
+            if(timeToShow <= 0)
+            {
+
+                panel.SetActive(false);
+            }
+        }
+
         if(distance > 1000)
         {
             PlayerPrefsManager.setDistance(distance);
             SceneManager.LoadScene(3);
         }
 	}
+
 }
